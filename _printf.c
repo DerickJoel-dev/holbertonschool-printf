34df84a05f3dc26395272e462c3693dc1ffd9346
DerickJoel-dev <derickdev2@gmail.com>
2024-11-30 15:20:29 -0400

    func produces output according to a format

---
+++ _printf.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * _printf - prints everything.
+ * @format: format string.
+ * Return: the count of printed characters.
+ */
+
+int _printf(const char *format, ...)
+{
+	int i = 0;
+	int count = 0;
+	va_list arg;
+	int (*func_ptr)(va_list);
+
+	if (format == NULL)
+	{
+		return (-1);
+	}
+	va_start(args, format);
+	if (*format)
+	{
+		while (format[i])
+		{
+			if (format[i] == '%' && (!format[i + 1] == '\0'))
+				return (-1);
+			if (format[i] == '%')
+			{
+				func_ptr = get_func(format[i + 1]);
+				if (func_ptr)
+				{
+					count += func_ptr(args);
+					i += 2;
+					continue;
+				}
+			}
+			_putchar(format[i]);
+			i++;
+			count++;
+		}
+	}
+	va_end(args);
+	return (count);
+}
