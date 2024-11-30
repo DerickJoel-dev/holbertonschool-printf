db74473c9272138c62ce863970a4021c6c9a267c
DerickJoel-dev <derickdev2@gmail.com>
2024-11-30 14:05:57 -0400

    prototype and functions

---
+++ main.h
@@ -0,0 +1,24 @@
+#ifndef PRINTF
+#define PRINTF
+#include <stdarg.h>
+#include <stddef.h>
+/**
+ * struct type_s - struct type_s
+ * @opt: options
+ * @f: the associated function
+ */
+typedef struct type_s
+{
+  char opt;
+  int (*f)(va_list);
+} type_t;
+
+int (*get_func(char s))(va_list);
+int _putchar(char c);
+int print_percent(va_list args);
+int print_c(va_list args);
+int print_s(va_list args);
+int print_d(va_list args);
+int _printf(const char *format, ...);
+
+#endif
